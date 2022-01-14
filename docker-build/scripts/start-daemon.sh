07029f39fe659ddd8568cbff1e6ca12125782130
lovehzj <1922919664@qq.com>
2022-01-14 20:11:51 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $FILEBEAT_HOME/logs
+mkdir -p $FILEBEAT_HOME/logs
 nohup $FILEBEAT_HOME/mate/filebeat_mate >>$FILEBEAT_HOME/filebeat_mate.stdout.log 2>>$FILEBEAT_HOME/filebeat_mate.stderr.log
 
--- docker-build/scripts/start-filebeat.sh
+++ docker-build/scripts/start-filebeat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $FILEBEAT_HOME/logs
+mkdir -p $FILEBEAT_HOME/logs
 nohup $FILEBEAT_HOME/filebeat -e -c $FILEBEAT_HOME/filebeat.yml >>$FILEBEAT_HOME/logs/filebeat.stdout.log 2>>$FILEBEAT_HOME/logs/filebeat.stderr.log &
 
